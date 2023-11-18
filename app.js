6087db677ed238db96aab7373a288f9b38dbde92
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2023-11-18 19:30:50 +0530

    invader created

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ class Player {
         const image = new Image()
         image.src = 'assests/spaceShip.png'
         image.onload = () => {
-            const scale = 0.17
+            const scale = 0.15
             this.image = image
             this.width = image.width * scale
             this. height = image.height * scale
@@ -89,12 +89,62 @@ class Projectile {
 }
 
 
+class Invader {
+    constructor(){
+        
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+
+        const image = new Image()
+        image.src = 'assests/invader.png'
+        image.onload = () => {
+            const scale = 0.15
+            this.image = image
+            this.width = image.width * scale
+            this. height = image.height * scale
+            this.position = {
+                x: canvas.width /2 - this.width/2,
+                y: canvas.height/2
+            }
+        }
+    }
+
+    draw() {
+       /*  c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height ) */
+
+
+
+        
+
+        c.drawImage
+        (this.image,
+        this.position.x,
+        this.position.y, 
+        this.width, 
+        this.height
+        )
+     
+    }
+
+    update() {
+        if (this.image) {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+        }
+    }
+
+}
 
 
 const player = new Player()
-const projectiles = [
-  
-]
+const projectiles = []
+const invader = new Invader() 
+
 const keys = {
     ArrowLeft:{
         pressed: false
@@ -113,8 +163,18 @@ function animate() {
     requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
+    invader.update()
     player.update()
-    projectiles.forEach(projectile => {
+    projectiles.forEach((projectile, index) => {
+
+        if(projectile.position.y + projectile.radius <= 0){
+            setTimeout(() => {
+                projectile.splice(index, 1)
+            }, 0)
+           
+        } else {
+            projectile.update()
+        }
         projectile.update()
     })
 
